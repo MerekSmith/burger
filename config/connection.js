c02fbc89276f8fdc48eac42f7ef19a7c3f62a36b
Merek Smith <mereksmith@hotmail.com>
2018-05-12 09:42:59 -0600

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 var mysql = require("mysql");
-
+var connection;
 // process.env.JAWSDB.URL
-
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-// {
-//   host: "localhost",
-//   user: "root",
-//   password: "17Lily!7h",
-//   database: "burgers_db"
-// });
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "17Lily!7h",
+  database: "burgers_db"
+});
+};
 
 connection.connect(function(err) {
   if (err) {
